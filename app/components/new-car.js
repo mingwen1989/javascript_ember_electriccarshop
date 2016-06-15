1e270767b6bb5bf645924d9d052e89a0cfd055ef
Kevin Deganos and Ming Wen <ming@>
2016-06-15 11:52:31 -0700

    implement add car form

---
+++ app/components/new-car.js
@@ -0,0 +1,17 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  saveCar() {
+    var params = {
+      make: this.get('make'),
+      model: this.get('model'),
+      price: this.get('price'),
+      range: this.get('range'),
+      description: this.get('description'),
+      power: this.get('power'),
+      type: this.get('type'),
+      image: this.get('image'),
+    };
+    this.sendAction('saveCar', params);
+  }
+});
--- app/routes/index.js
+++ app/routes/index.js
@@ -3,5 +3,12 @@ import Ember from 'ember';
 export default Ember.Route.extend({
     model: function() {
       return this.store.findAll('car');
+    },
+  actions: {
+    saveCar(params) {
+    var newCar = this.store.createRecord('car', params);
+    newCar.save();
+    this.transitionTo('index');
     }
+  }
 });
