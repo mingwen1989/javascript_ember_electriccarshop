cb4d3034e36cc8b6e9e5ec7552ded1429532e482
Kevin Deganos and Ming Wen <kevin@>
2016-06-16 16:36:18 -0700

    add average rating for each car

--- app/components/car-info.js
+++ app/components/car-info.js
@@ -2,6 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   shoppingCart: Ember.inject.service(),
+  averageRating: 0,
 
   actions: {
     addToCart(car) {
@@ -18,5 +19,35 @@ export default Ember.Component.extend({
     saveComment(params) {
       this.sendAction('saveComment', params);
     }
+  },
+
+  // computed property method
+  // userRating: Ember.computed('car.comments.[]', function() {
+  //   // var self = this;
+  //   var ratingCount = 0;
+  //   var totalRatings = this.get('car.comments.length');
+  //   return this.get('car.comments').then((comments) => {
+  //     comments.forEach((comment) => {
+  //       ratingCount += parseInt(comment.get('rating'));
+  //     });
+  //     var avg = ratingCount / totalRatings;
+  //     console.log("inside computed property: " + avg);
+  //     return avg;
+  //     // self.set('averageRating', avg);
+  //   });
+  // }),
+
+  // manually setting property once page is renderedd
+  didInsertElement() {
+    var self = this;
+    var ratingCount = 0;
+    var totalRatings = this.get('car.comments.length');
+    return this.get('car.comments').then((comments) => {
+      comments.forEach((comment) => {
+        ratingCount += parseInt(comment.get('rating'));
+      });
+      self.set('averageRating', ratingCount / totalRatings);
+    });
   }
+
 });
